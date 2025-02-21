d56e5dc29e771c25ad3ce041bccb6e89e9a16bc1
Luanqpa <rastello.boulot@gmail.com>
2025-02-21 10:16:33 +0100

    kirby movement

--- Assets/Script/Kirby.cs
+++ Assets/Script/Kirby.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class KirbyMovement : MonoBehaviour
 {
-    public float KirbyMovementSpeed = 5f; //speeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed
+    public float KirbyMovementSpeed = 5f; //speeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed
     private float horizontalInput;
     private int currentX = 0;
-    
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        MoveKirby();
+    }
+
+    private void MoveKirby()
+    {
+        Vector3 movement = new Vector3(horizontalInput, 0, 0) * KirbyMovementSpeed * Time.deltaTime;
+        transform.position += movement;
+        currentX += (int)horizontalInput;
+    }
 }
+
 
 public class KirbyHp : MonoBehaviour
 {
     public int kirbyHp = 1;
+
+    public void TakeDamage(int damage)
+    {
+        
+    }
+
+
+
+    public void Die()
+    {
+        Debug.Log("skill issues")
+    }
 }
 
 public class KirbyFireRate : MonoBehaviour
 {
-    public int kirbyFireRate = 3;  // lorsque j'appui sur espace, je tire en continue x balles par sec
+    public int kirbyFireRate = 3;  // lorsque j'appui sur espace, je tire en continue x balles par sec, en vrai jsp
 }
